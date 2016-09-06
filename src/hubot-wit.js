f942f67366a898f6101cb42d7a72e9de5d580d41
Adriano Godoy <godoy.ccp@gmail.com>
2016-09-06 15:39:24 -0300

    First functional version integrated with hubot

---
+++ src/hubot-wit.js
@@ -0,0 +1,48 @@
+// Description
+//   Talking with a Wit robot
+//
+// Configuration:
+//   WIT_TOKEN
+//
+// Commands:
+//   None
+//
+// Notes:
+//   <optional notes required for the script>
+//
+// Author:
+//   Adriano Godoy
+
+const {Wit, log, interactive} = require('node-wit');
+const WIT_TOKEN = process.env.WIT_TOKEN;
+
+module.exports = function(robot) {
+
+  robot.respond(/(.*)/i, function(res) {
+    const query = res.match[1];
+    let context = {};
+
+    const actions = {
+      send(request, response) {
+        const {sessionId, context, entities} = request;
+        const {text, quickreplies} = response;
+
+        return new Promise(function(resolve, reject) {
+          res.reply(text);
+          return resolve();
+        });
+      },
+    };
+
+    const client = new Wit({accessToken: WIT_TOKEN, actions});
+
+    client.runActions("session-" + res.message.user["id"], query, context)
+      .then((ctx) => {
+        context = ctx;
+      })
+      .catch(err => console.error(err))
+
+    return;
+  });
+
+};
